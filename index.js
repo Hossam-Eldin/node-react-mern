e73c23c673fb4d2145bf151b90ef20691631bca0
sam <Hossam-Elden>
2019-02-10 12:37:08 +0200

    index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const cookieSession =require('cookie-session')
 const passport = require('passport')
+const authRoutes = require('./routes/authRoutes')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 // must be required first or it will create error 
 require('./models/User')
 //require it after the requireing User Model 
 require('./services/passport')
-const authRoutes = require('./routes/authRoutes')
-const mongoose = require('mongoose')
-const keys = require('./config/keys')
 
 //data base connection 
 mongoose.connect(keys.databaseURI, { useNewUrlParser: true })
