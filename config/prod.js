1ae2c48c0430a047d5ec18d9e69d1c3f0596f853
sam <Hossam-Elden>
2019-02-10 12:12:56 +0200

    client

--- config/prod.js
+++ config/prod.js
@@ -4,3 +4,11 @@ module.exports ={
     databaseURI       : process.env.MONGO_URI,
     cookieKey         : process.env.COOKIE_KEY
 };
+
+
+/**
+ * 165055035440-01v83feb3ng2tm8p5lcqoj40s2f7rkoq.apps.googleusercontent.com  client id
+ * PNCmSIsYb3i8fyZ8DTyr-Hj5   client id
+ * 
+ * mongodb://mern-prod-user:123321prod@ds127535.mlab.com:27535/mern-prod
+ */
